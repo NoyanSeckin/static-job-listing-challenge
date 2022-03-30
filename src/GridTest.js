4b98554b63db23f71b8ff4d37c3fd820f121406c
FKNoyan <egemennoyan@hotmail.com>
2022-03-30 14:23:30 +0300

    dekstop version completed

--- src/GridTest.js
+++ src/GridTest.js
@@ -3,32 +3,40 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
+// import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+// import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import ClearIcon from '@mui/icons-material/Clear';
 import {UserData} from "./data";
 // import Manage from "./FrontendMentorFiles/images/manage.svg";
 export default function GridTest() {
+ 
 
   const [searchList, setSearchList] = useState([]);
+  const deleteFromSearchList = (listItem) => {
+    const newSearchList = searchList.filter((el) => el !== listItem);
+    setSearchList(newSearchList);
+  }
   const renderSearchBar = () => {
     return(
       <Grid container justifyContent={"space-between"} className={"search-bar"} xs={
         9} sx={{px: 4}}>
        <Grid item sx={{display: "flex"}} gap={1}  >
-        {searchList.map(listItem => (
-            <Box sx={{display: "flex"}}>
-            <Typography className={"attribute-tag"} variant="body2">
-              {listItem} 
-            </Typography> 
-            <ClearIcon onClick={()=> setSearchList(searchList => [searchList.filter(el => el !== listItem)]) }></ClearIcon>
-            </Box>
-          ))}
+        {searchList.map((listItem) => {
+          return (
+            <Box key={listItem} sx={{display: "flex"}}>
+              <Typography className={"searchlist-tag"} variant="caption">
+                {listItem} 
+              </Typography> 
+              <ClearIcon className={"clear-icon"}
+              onClick={() => deleteFromSearchList(listItem)}>
+              </ClearIcon>
+            </Box>)
+        })}
        </Grid>
        <Grid item   >
-        <Typography variant="body1">
+        <Typography className={"clear-tag"} onClick={()=> setSearchList([])} variant="caption">
           Clear
         </Typography>
        </Grid>
@@ -39,15 +47,13 @@ export default function GridTest() {
   const addToSearchbar = (item) => {
    if(!searchList.includes(item)){
      setSearchList(searchList => [...searchList, item] )
-   }else{
-     console.log("Cannot add ",item);
    }
   }
   const renderCardsTop = (userCompany, userNew, userFeatured) =>{
     return(
       <Grid container>
         <Typography variant="caption" display="block" gutterBottom>
-                    {userCompany}
+          {userCompany}
         </Typography>
          {userNew && (
            <Typography
@@ -89,7 +95,7 @@ export default function GridTest() {
       </Grid>
     )
   }
-  const renderCardsRightSide = (userRole, userLevel, userLanguages) => {
+  const renderCardsRightSide = (userRole, userLevel, userLanguages, userTools) => {
     return(
       <Grid
       container
@@ -102,30 +108,43 @@ export default function GridTest() {
       <Typography onClick={ ()=> addToSearchbar(userRole)} className={"attribute-tag"} variant="caption">
         {userRole}
       </Typography>
-      <Typography className={"attribute-tag"} variant="caption">
+      <Typography onClick={ ()=> addToSearchbar(userLevel)} className={"attribute-tag"} variant="caption">
         {userLevel}
       </Typography>
       {Object.values(userLanguages).map((value) => (
-        <Typography className={"attribute-tag"}  variant="caption">
+        <Typography onClick={ ()=> addToSearchbar(value)}  className={"attribute-tag"}  variant="caption">
           {value}
         </Typography>
+      ))}
+      {Object.values(userTools).map((value) => (
+         <Typography onClick={ ()=> addToSearchbar(value)}  className={"attribute-tag"}  variant="caption">
+         {value}
+       </Typography>
       ))}
     </Grid>
     )
   }
 
-  const renderUserData = () => {
-    // let filteredData = UserData.filter((user) => user.languages.includes() || user.role);
-    return UserData.map((user) => (
+  const renderUserCard = () => {
+    // searchList.every(value => jobTags.includes(value))
+    let filteredData = []
+    UserData.forEach(user => {
+      let userTags = [user.position, user.level, user.contract, user.role, ...user.languages, ...user.tools];
+      // compare every value 
+      if(searchList.every(value => userTags.includes(value))){
+        filteredData.push(user)
+      }
+    })
+    return filteredData.map((user) => (
       <Grid className={"card"} item xs={9}>
-        <Paper sx={{display: 'flex', py:2}}>
+        <Paper sx={{display: 'flex',  py:2}}>
          <img className={"user-logo"} src={user.logo.default} alt="sa"/>
             <Grid container  xs={6}>
               {renderCardsTop(user.company, user.new, user.featured)}
               <Typography variant="body2">{user.position}</Typography>
               {renderCardsBottom(user.postedAt, user.contract, user.location)}
             </Grid>
-            {renderCardsRightSide(user.role, user.level, user.languages)}
+            {renderCardsRightSide(user.role, user.level, user.languages, user.tools)}
         </Paper>
       </Grid>
     ));
@@ -133,11 +152,11 @@ export default function GridTest() {
   };
   return (
     <div>
-      { searchList.length > 0 && renderSearchBar()}
        {/* Cards container */}
-      <Box sx={{ mt: 4 }}>
+      <Box  sx={{ mt: 4 }}>
+        { searchList.length > 0 && renderSearchBar()}
         <Grid container gap={2} justifyContent="center">
-          {renderUserData()}
+          {renderUserCard()}
         </Grid>
       </Box>
     </div>
